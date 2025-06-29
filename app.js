723a1f51bb42d4240586cf752ab562065d6f946b
Denner Cardoso <denner_ttpc@hotmail.com>
2025-06-28 18:21:46 -0700

    begin setting up db models/schemas

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// listen to port 3000
+const app = express();
+const { PORT = 3001 } = process.env;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
+
+app.listen(PORT, () => {
+  // if everything works fine, the console will show which port the application is listening to
+  console.log(`App listening at port ${PORT}`);
+});
