af3d82fe49cfd7382b00d2d5bd01963652d62093
Denner Cardoso <denner_ttpc@hotmail.com>
2025-06-29 20:13:35 -0700

    set up and test users controller

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,15 @@ const User = require("../models/user");
 
 const checkResponse = (res, err) => {
   console.error(err);
+  if (err.name === "ValidationError") {
+    return res.status(400).send({ message: err.message });
+  }
+  if (err.name === "CastError") {
+    return res.status(400).send({ message: "Invalid ID format" });
+  }
+  if (err.name === "DocumentNotFoundError") {
+    return res.status(404).send({ message: "User not found" });
+  }
   return res.status(500).send({ message: err.message });
 };
 
@@ -18,4 +27,12 @@ const createUser = (req, res) => {
     .catch((err) => checkResponse(res, err));
 };
 
-module.exports = { getUsers, createUser };
+const getUser = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .orFail()
+    .then((user) => res.status(200).send(user))
+    .catch((err) => checkResponse(res, err));
+};
+
+module.exports = { getUsers, createUser, getUser };
